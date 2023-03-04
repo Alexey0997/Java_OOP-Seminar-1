dd4a95b984890acc24be1387926f1d5b9f85923d
alexey <asivlev@yandex.ru>
2023-03-05 01:42:42 +0300

    Food class created

--- Food.java
+++ Food.java
@@ -1,18 +1,15 @@
 public class Food extends Product{
-    String expirationDate;
+    String expirationDate;                             // Переменная срока годности.
     public Food(String name, int price, int numbers, String measureUnit, String expirationDate) {
-        super(name, price, numbers, measureUnit);
-        this.expirationDate = expirationDate;
+        super(name, price, numbers, measureUnit);      // Импорт данных из родительского класса Program.
+        this.expirationDate = expirationDate;          // Добавление переменной данного - наследуемого класса.
 
     }
-    public String toString(){
-
+    public String toString(){                          // Метод вывода на печать сведений о продуктах данного класса.
         return "\nНаименование товара: " + name +
                 "\nЦена, рублей:        " + price +
                 "\nКоличество:          " + numbers +
                 "\nЕдиницы измерения:   " + measureUnit +
                 "\nСрок годности:       " + expirationDate;
     }
-
-
 }
